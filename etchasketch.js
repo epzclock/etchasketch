294f17489846e6a72a1e66f095a97eecc9997fc6
epzclock <edwardjjz@hotmail.com>
2019-08-26 02:40:30 -0700

    Added hovering and changing color of each box

--- etchasketch.js
+++ etchasketch.js
@@ -7,9 +7,18 @@
     for (let j = 1; j < 17; j++) {
       let squares = document.createElement("div");
       squares.classList.add("gridsquares");
+      squares.addEventListener("mouseover", event => {
+        console.log(
+          (event.target.style = `background-color: #${Math.floor(
+            Math.random() * 10
+          )}${Math.floor(Math.random() * 10)}${Math.floor(
+            Math.random() * 10
+          )}${Math.floor(Math.random() * 10)}${Math.floor(
+            Math.random() * 10
+          )}${Math.floor(Math.random() * 10)};`)
+        );
+      });
       row.appendChild(squares);
     }
   }
 })();
-
-
