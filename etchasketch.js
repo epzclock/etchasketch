acdd8bbc1880873928758ebbc5c949e91f0aad64
epzclock <edwardjjz@hotmail.com>
2019-08-26 03:38:35 -0700

    added two buttons with functionality on reset, working on newgrid

--- etchasketch.js
+++ etchasketch.js
@@ -1,5 +1,13 @@
 (() => {
   const container = document.getElementById("container");
+  let resetbutton = document.createElement("button");
+  let newgrid = document.createElement("button");
+  resetbutton.innerHTML = "Reset";
+  newgrid.innerHTML = "New Grid";
+  container.append(resetbutton);
+  container.append(newgrid);
+  newgrid.id = "new-grid";
+  resetbutton.id = "reset";
   for (let i = 1; i < 17; i++) {
     let row = document.createElement("div");
     row.classList.add(`row`);
@@ -22,3 +30,13 @@
     }
   }
 })();
+
+let resetButton = document.getElementById("reset");
+
+resetButton.addEventListener("click", () => {
+  let gs = document.getElementsByClassName("gridsquares");
+  for (let i = 0; i < gs.length; i++) {
+    gs[i].style = `background-color: rgb(240, 227, 227)`;
+  }
+  //   gs.style = `background-color: rgb(240, 227, 227)`;
+});
