0219dfdd96b8ff07da5bec5fa671713a6a2ea6f1
epzclock <edwardjjz@hotmail.com>
2019-08-26 04:20:53 -0700

    Added New Grid functionality, bugs still present with large grids

--- etchasketch.js
+++ etchasketch.js
@@ -32,11 +32,41 @@
 })();
 
 let resetButton = document.getElementById("reset");
-
-resetButton.addEventListener("click", () => {
+const resetListener = resetButton.addEventListener("click", () => {
   let gs = document.getElementsByClassName("gridsquares");
   for (let i = 0; i < gs.length; i++) {
     gs[i].style = `background-color: rgb(240, 227, 227)`;
   }
-  //   gs.style = `background-color: rgb(240, 227, 227)`;
+});
+
+let newgridButton = document.getElementById("new-grid");
+
+newgridButton.addEventListener("click", () => {
+  let rows = document.querySelectorAll(".row");
+  console.log(rows);
+  for (let i = 0; i < rows.length; i++) {
+    rows[i].remove();
+  }
+  let size = prompt("How large do you want the grid?");
+  for (let i = 1; i < size; i++) {
+    let row = document.createElement("div");
+    row.classList.add(`row`);
+    container.appendChild(row);
+    for (let j = 1; j < size; j++) {
+      let squares = document.createElement("div");
+      squares.classList.add("gridsquares");
+      squares.addEventListener("mouseover", event => {
+        console.log(
+          (event.target.style = `background-color: #${Math.floor(
+            Math.random() * 10
+          )}${Math.floor(Math.random() * 10)}${Math.floor(
+            Math.random() * 10
+          )}${Math.floor(Math.random() * 10)}${Math.floor(
+            Math.random() * 10
+          )}${Math.floor(Math.random() * 10)};`)
+        );
+      });
+      row.appendChild(squares);
+    }
+  }
 });
